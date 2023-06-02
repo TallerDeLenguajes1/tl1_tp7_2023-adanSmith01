0d7866108fd76fbbde7beb8413843fb9f9bc7220
AdanSmith <smith.paredes.13@gmail.com>
2023-06-02 15:27:11 -0300

    clase Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,27 @@
+namespace spaceCalc;
+
+public class Calculadora{
+    private double resultado;
+
+    public double Resultado { get => resultado;}
+
+    public void Sumar(double termino){
+        resultado += termino;
+    }
+
+    public void Restar(double termino){
+        resultado -= termino;
+    }
+
+    public void Multiplicar(double termino){
+        resultado *= termino;
+    }
+
+    public void Dividir(double termino){
+        resultado /= termino;
+    }
+
+    public void Limpiar(){
+        resultado = 0;
+    }
+}
